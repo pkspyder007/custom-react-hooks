7516717be512e0fac366c5e5965b0eb7329a8b8d
pkpsyder007 <pkpsyder007@gmail.com>
2020-10-19 21:27:36 +0530

    INITIAL COMMIT

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Custom Hook Series</h1>
+      <h2>Let's create some useful hooks in React.JS!</h2>
+      <div className="container">
+        <span>X</span>
+        <span> - </span>
+        <span>Y</span>
+      </div>
+    </div>
+  );
+}
