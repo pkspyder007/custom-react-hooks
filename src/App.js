9778aa64f43186b7123a6df4ddf7e73b75e78aaf
Praveen Kumar <pkspyder007@gmail.com>
2020-10-28 23:14:59 +0530

    Added useNetworkStatus hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import useLocalStorage from "./hooks/useLocalStorage";
+import useNetworkStatus from "./hooks/useNetworkStatus";
 import "./styles.css";
 
 export default function App() {
   const [isDark, setIsDark] = useLocalStorage("isDark", false);
   console.log(isDark);
-  
+  const status = useNetworkStatus();
   return (
     <div className={`App ${isDark ? "dark" : ""}`}>
       <h1>Custom Hook Series</h1>
       <h2>Let's create some useful hooks in React.JS!</h2>
       <div className="container">
-        <span>Persist State to localStorage</span>
+        <span className={`status ${status ? "online" : "offline"}`}>
+          {status ? "Online" : "Offline"}
+        </span>
       </div>
       <br />
       <button onClick={() => setIsDark(!isDark)}>Toggle Theme </button>
